54e6f15511bd33024436ce89179baa8b212d5200
ivandiassantos <ivan.dias.santos@gmail.com>
2019-03-26 18:35:08 -0300

    Update ClienteController.java
    Correção no cadastro de clientes.

--- src/main/java/br/com/cadastroclientes/controller/ClienteController.java
+++ src/main/java/br/com/cadastroclientes/controller/ClienteController.java
@@ -4,22 +4,26 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cadastroclientes.model.Cliente;
+import br.com.cadastroclientes.model.Endereco;
+import br.com.cadastroclientes.model.Telefone;
 import br.com.cadastroclientes.service.ClienteService;
 
 @RestController
-@RequestMapping("/v1/cliente")
+@RequestMapping(value = "/v1/cliente")
 public class ClienteController {
 	@Autowired
 	private ClienteService clienteService;
 	
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Cliente> cadastrar(Cliente cliente){
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Cliente> cadastrar(@RequestBody Cliente cliente){
 		clienteService.salvar(cliente);
 		return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
 	}
